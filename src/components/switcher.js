62485864e98925c698ea49ca09691fab24d7e6ed
Maartje Eyskens <meyskens@me.com>
2018-01-24 12:50:38 +0100

    add live updates

--- src/components/switcher.js
+++ src/components/switcher.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, Row, Col } from 'react-materialize'
+import io from 'socket.io-client';
 import { getAllNews } from '../apis/news_api'
 import NewsItem from './newsitem'
 import newsitem from './newsitem';
@@ -8,12 +9,20 @@ class Switcher extends Component {
     constructor(props) {
         super(props)
 
+        // connect live reloader
+        const socket = io.connect("http://localhost:8080/")
+        socket.on('update', this.loadNews.bind(this))
+    
         this.state = { 
             items: [] ,
         }
-
-        getAllNews().then(this.gotNewsItems.bind(this))
+        
+        this.loadNews.bind(this)()         
         setInterval(this.rotate.bind(this), 5000)
+    }
+
+    loadNews() {
+        getAllNews().then(this.gotNewsItems.bind(this))
     }
 
     gotNewsItems(result) {
