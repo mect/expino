8a098683527e689e8924affcf077a5473e4fdaa0
Dirk Mervis <dirk.mervis@gmail.com>
2018-01-25 15:37:03 +0100

    nu met alles erbij

--- src/apis/news_api.js
+++ src/apis/news_api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const HOST = "http://localhost:8080"
+const HOST = "http://192.168.99.100:8080";
 
-export const getAllNews = () => axios.get(`${HOST}/api/news`)
+export const getAllNews = () => axios.get(`${HOST}/api/news`);
--- src/components/switcher.js
+++ src/components/switcher.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import { Card, Row, Col } from 'react-materialize'
+import { Card, Row, Col, Icon } from 'react-materialize'
 import { getAllNews } from '../apis/news_api'
-import NewsItem from './newsitem'
-import newsitem from './newsitem';
+import NewsItem from './newsitem';
+import '../css/switcher.css';
+import { LOGO } from '../variables';
 
 class Switcher extends Component {
     constructor(props) {
-        super(props)
+        super(props);
 
         this.state = { 
             items: [] ,
-        }
+        };
 
-        getAllNews().then(this.gotNewsItems.bind(this))
+        getAllNews().then(this.gotNewsItems.bind(this));
         setInterval(this.rotate.bind(this), 5000)
     }
 
@@ -21,10 +22,10 @@ class Switcher extends Component {
     }
 
     rotate() {
-        let items = this.state.items
-        const lastZero = items[0]
-        items = items.slice(1,items.length)
-        items.push(lastZero)
+        let items = this.state.items;
+        const lastZero = items[0];
+        items = items.slice(1,items.length);
+        items.push(lastZero);
 
         this.setState({ items })
     }
@@ -33,18 +34,24 @@ class Switcher extends Component {
         return <div>
             <Row>
             <Col s={2}>
-                <Row><Card title="Up next">{this.state.items[1]}</Card></Row>
-                <Row><Card title="Up next next">{this.state.items[2]}</Card></Row>
-                <Row><Card/></Row>
-                <Row><Card/></Row>
-                <Row><Card/></Row>
+                <Row><div className="logo-margin"><img className="logo" src={LOGO} alt="zorginnovatie logo"/></div></Row>
+                <Row><Card className="left-column-card"></Card></Row>
+                <Row><Card className="left-column-card"></Card></Row>
+                <Row><Card className="left-column-card">{this.state.items[2]}</Card></Row>
+                <Row><Card className="left-column-card">
+                    <div className="up-next-title">Up Next:</div>
+                    {this.state.items[1]}</Card></Row>
             </Col>
             <Col s={10}>
-                <Card>{this.state.items[0]}</Card>
+                <Card className="mainslide">{this.state.items[0]}</Card>
             </Col>
         </Row>
-        <Row>
-            <Card>Expino + 100% - BTC -50% - TM -100%</Card>
+        <Row className="ticker-margin">
+            <Card className="ticker-card-style">
+                <span className="ticker-up ticker-item-width"><Icon className="ticker-up-arrow">   forward</Icon> Expino + 100%   </span>
+                <span className="ticker-down ticker-item-width"><Icon className="ticker-down-arrow">   forward</Icon> BTC -50%   </span>
+                <span className="ticker-down ticker-item-width"><Icon className="ticker-down-arrow">   forward</Icon> TM -100%   </span>
+            </Card>
         </Row>
         </div>
 
