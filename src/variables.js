ebf026fe6e239dc9b7345858e1f4a4a9212e4225
Maartje Eyskens <meyskens@me.com>
2018-02-21 19:55:10 +0100

    add host

--- src/variables.js
+++ src/variables.js
@@ -4,3 +4,4 @@
 export const LOGO = 'http://projecten.dirkmervis.be/expino/zorg_logo.png';
 export const THEMECOLOR = 'rgb(231,104,0)';
 export const HOST = 'http://192.168.99.100:8080'
+export const TICKCER_HOST = `http://${window.location.hostname}:8070`
